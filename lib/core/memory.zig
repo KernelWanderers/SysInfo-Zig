e8826dca653b42d17866dae4cd85d7f07b4980a6
ObscureNightingale <twitchisadeck@gmail.com>
2021-11-27 19:20:04 +0100

    update: use doc comments instead of regular comments (oops)

--- lib/core/memory.zig
+++ lib/core/memory.zig
@@ -1,45 +1,45 @@
-const MemoryModuleSlot = struct {
-    // The bank index this RAM module is located in.
+const MemoryModuleSlot = extern struct {
+    /// The bank index this RAM module is located in.
     bank: u32,
 
-    // The specific channel index this RAM module is located in,
-    // relative to its bank location.
+    /// The specific channel index this RAM module is located in,
+    /// relative to its bank location.
     channel: u32
 };
 
 const MemoryModule = struct {
-    // The size of this RAM module.
+    /// The size of this RAM module.
     size: u32,
 
-    // The slot this RAM module is located in.
+    /// The slot this RAM module is located in.
     slot: MemoryModuleSlot,
 
-    // The serial number of this RAM module.
-    // Aliases: `serialNumber`
+    /// The serial number of this RAM module.
+    /// Aliases: `serialNumber`
     sn: *const []u8,
 
-    // The serial number of this RAM module.
-    // Aliases: `sn`
+    /// The serial number of this RAM module.
+    /// Aliases: `sn`
     serialNumber: *const []u8,
 
-    // The part number of this RAM module.
+    /// The part number of this RAM module.
     partNumber: *const []u8,
 
-    // The type of this RAM module.
-    // For example, DDR4, DDR3, DDR2, etc.
+    /// The type of this RAM module.
+    /// For example, DDR4, DDR3, DDR2, etc.
     module_type: *const []u8,
 
-    // The manufacturer of this RAM module.
+    /// The manufacturer of this RAM module.
     manufacturer: *const []u8
 };
 
 const RAMData = struct {
-    // The number of RAM modules in this data structure.
+    /// The number of RAM modules in this data structure.
     count: u32,
 
-    // A slice of memory modules available..
+    /// A slice of memory modules available..
     modules: *const MemoryModule,
 
-    // The total size of RAM installed.
+    /// The total size of RAM installed.
     totalSize: u32
 };
