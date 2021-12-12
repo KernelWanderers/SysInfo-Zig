0c0b308674b799630c08be8db235f96210d0e096
ObscureNightingale <twitchisadeck@gmail.com>
2021-12-12 23:57:50 +0100

    add: unit tests

--- build.zig
+++ build.zig
@@ -27,8 +27,10 @@ pub fn build(b: *std.build.Builder) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_tests = b.addTest("lib/main.zig");
+    const exe_tests = b.addTest("tests.zig");
     exe_tests.setBuildMode(mode);
+
+    deps.addAllTo(exe_tests);
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&exe_tests.step);
